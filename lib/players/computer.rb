dc8fefa17a463624a531964f1572c3a4f5abbc2c
Conor Hamilton <conhamil19@gmail.com>
2018-06-05 13:41:41 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -54,7 +54,7 @@ class Players
     def can_i_lose?
       if self.token == "O"
       lose_combo = WIN_COMBINATIONS.detect do |c|
-      (@board.cells[c[0]] != "X" && @board.cells[c[1]] != "X" && @board.cells[c[2]] == " ") ||
+      (@board.cells[c[0]] == "X" && @board.cells[c[1]] != "X" && @board.cells[c[2]] == " ") ||
       (@board.cells[c[0]] != "X" && @board.cells[c[2]] != "X" && @board.cells[c[1]] == " ") ||
       (@board.cells[c[1]] != "X" && @board.cells[c[2]] != "X" && @board.cells[c[0]] == " ")
       end 
